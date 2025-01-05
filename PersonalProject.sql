146baf4dad900f600775d1cf5a34aaca10343d61
SAI SNEHANTH.C <134220516+Sai-snehanth@users.noreply.github.com>
2025-01-05 10:02:46 +0530

    Add files via upload

---
+++ PersonalProject.sql
@@ -0,0 +1,270 @@
+use personal_project;
+select * from death_rates_usa;
+select * from death_rates_usa where Year=1900;
+ALTER TABLE death_rates_usa 
+CHANGE `Leading Causes` Leading_Causes VARCHAR(255);
+
+ALTER TABLE death_rates_usa 
+CHANGE `Age Adjusted Death Rate` AgeAdjustedDeathRate VARCHAR(255);
+
+select * from death_rates_usa where Leading_Causes="Heart Disease" and Year=1900;
+
+select * from death_rates_usa where Leading_Causes="Heart Disease";
+
+select * from death_rates_usa where Leading_Causes="Cancer";
+
+select * from death_rates_usa where Leading_Causes="Stroke";
+
+select * from death_rates_usa where Leading_Causes="Accidents";
+
+select * from death_rates_usa where Leading_Causes="Influenza and Pneumonia";
+
+select distinct Leading_Causes as Causes from death_rates_usa;
+
+select distinct Year as years from death_rates_usa;
+
+SELECT * FROM death_rates_usa WHERE Year = 1900;
+
+SELECT * FROM death_rates_usa WHERE Year = 1901;
+
+SELECT * FROM death_rates_usa WHERE Year = 1902;
+
+SELECT * FROM death_rates_usa WHERE Year = 1903;
+
+SELECT * FROM death_rates_usa WHERE Year = 1904;
+
+SELECT * FROM death_rates_usa WHERE Year = 1905;
+
+SELECT * FROM death_rates_usa WHERE Year = 1906;
+
+SELECT * FROM death_rates_usa WHERE Year = 1907;
+
+SELECT * FROM death_rates_usa WHERE Year = 1908;
+
+SELECT * FROM death_rates_usa WHERE Year = 1909;
+
+SELECT * FROM death_rates_usa WHERE Year = 1910;
+
+SELECT * FROM death_rates_usa WHERE Year = 1911;
+
+SELECT * FROM death_rates_usa WHERE Year = 1912;
+
+SELECT * FROM death_rates_usa WHERE Year = 1913;
+
+SELECT * FROM death_rates_usa WHERE Year = 1914;
+
+SELECT * FROM death_rates_usa WHERE Year = 1915;
+
+SELECT * FROM death_rates_usa WHERE Year = 1916;
+
+SELECT * FROM death_rates_usa WHERE Year = 1917;
+
+SELECT * FROM death_rates_usa WHERE Year = 1918;
+
+SELECT * FROM death_rates_usa WHERE Year = 1919;
+
+SELECT * FROM death_rates_usa WHERE Year = 1920;
+
+SELECT * FROM death_rates_usa WHERE Year = 1921;
+
+SELECT * FROM death_rates_usa WHERE Year = 1922;
+
+SELECT * FROM death_rates_usa WHERE Year = 1923;
+
+SELECT * FROM death_rates_usa WHERE Year = 1924;
+
+SELECT * FROM death_rates_usa WHERE Year = 1925;
+
+SELECT * FROM death_rates_usa WHERE Year = 1926;
+
+SELECT * FROM death_rates_usa WHERE Year = 1927;
+
+SELECT * FROM death_rates_usa WHERE Year = 1928;
+
+SELECT * FROM death_rates_usa WHERE Year = 1929;
+
+SELECT * FROM death_rates_usa WHERE Year = 1930;
+
+SELECT * FROM death_rates_usa WHERE Year = 1931;
+
+SELECT * FROM death_rates_usa WHERE Year = 1932;
+
+SELECT * FROM death_rates_usa WHERE Year = 1933;
+
+SELECT * FROM death_rates_usa WHERE Year = 1934;
+
+SELECT * FROM death_rates_usa WHERE Year = 1935;
+
+SELECT * FROM death_rates_usa WHERE Year = 1936;
+
+SELECT * FROM death_rates_usa WHERE Year = 1937;
+
+SELECT * FROM death_rates_usa WHERE Year = 1938;
+
+SELECT * FROM death_rates_usa WHERE Year = 1939;
+
+SELECT * FROM death_rates_usa WHERE Year = 1940;
+
+SELECT * FROM death_rates_usa WHERE Year = 1941;
+
+SELECT * FROM death_rates_usa WHERE Year = 1942;
+
+SELECT * FROM death_rates_usa WHERE Year = 1943;
+
+SELECT * FROM death_rates_usa WHERE Year = 1944;
+
+SELECT * FROM death_rates_usa WHERE Year = 1945;
+
+SELECT * FROM death_rates_usa WHERE Year = 1946;
+
+SELECT * FROM death_rates_usa WHERE Year = 1947;
+
+SELECT * FROM death_rates_usa WHERE Year = 1948;
+
+SELECT * FROM death_rates_usa WHERE Year = 1949;
+
+SELECT * FROM death_rates_usa WHERE Year = 1950;
+
+SELECT * FROM death_rates_usa WHERE Year = 1951;
+
+SELECT * FROM death_rates_usa WHERE Year = 1952;
+
+SELECT * FROM death_rates_usa WHERE Year = 1953;
+
+SELECT * FROM death_rates_usa WHERE Year = 1954;
+
+SELECT * FROM death_rates_usa WHERE Year = 1955;
+
+SELECT * FROM death_rates_usa WHERE Year = 1956;
+
+SELECT * FROM death_rates_usa WHERE Year = 1957;
+
+SELECT * FROM death_rates_usa WHERE Year = 1958;
+
+SELECT * FROM death_rates_usa WHERE Year = 1959;
+
+SELECT * FROM death_rates_usa WHERE Year = 1960;
+
+SELECT * FROM death_rates_usa WHERE Year = 1961;
+
+SELECT * FROM death_rates_usa WHERE Year = 1962;
+
+SELECT * FROM death_rates_usa WHERE Year = 1963;
+
+SELECT * FROM death_rates_usa WHERE Year = 1964;
+
+SELECT * FROM death_rates_usa WHERE Year = 1965;
+
+SELECT * FROM death_rates_usa WHERE Year = 1966;
+
+SELECT * FROM death_rates_usa WHERE Year = 1967;
+
+SELECT * FROM death_rates_usa WHERE Year = 1968;
+
+SELECT * FROM death_rates_usa WHERE Year = 1969;
+
+SELECT * FROM death_rates_usa WHERE Year = 1970;
+
+SELECT * FROM death_rates_usa WHERE Year = 1971;
+
+SELECT * FROM death_rates_usa WHERE Year = 1972;
+
+SELECT * FROM death_rates_usa WHERE Year = 1973;
+
+SELECT * FROM death_rates_usa WHERE Year = 1974;
+
+SELECT * FROM death_rates_usa WHERE Year = 1975;
+
+SELECT * FROM death_rates_usa WHERE Year = 1976;
+
+SELECT * FROM death_rates_usa WHERE Year = 1977;
+
+SELECT * FROM death_rates_usa WHERE Year = 1978;
+
+SELECT * FROM death_rates_usa WHERE Year = 1979;
+
+SELECT * FROM death_rates_usa WHERE Year = 1980;
+
+SELECT * FROM death_rates_usa WHERE Year = 1981;
+
+SELECT * FROM death_rates_usa WHERE Year = 1982;
+
+SELECT * FROM death_rates_usa WHERE Year = 1983;
+
+SELECT * FROM death_rates_usa WHERE Year = 1984;
+
+SELECT * FROM death_rates_usa WHERE Year = 1985;
+
+SELECT * FROM death_rates_usa WHERE Year = 1986;
+
+SELECT * FROM death_rates_usa WHERE Year = 1987;
+
+SELECT * FROM death_rates_usa WHERE Year = 1988;
+
+SELECT * FROM death_rates_usa WHERE Year = 1989;
+
+SELECT * FROM death_rates_usa WHERE Year = 1990;
+
+SELECT * FROM death_rates_usa WHERE Year = 1991;
+
+SELECT * FROM death_rates_usa WHERE Year = 1992;
+
+SELECT * FROM death_rates_usa WHERE Year = 1993;
+
+SELECT * FROM death_rates_usa WHERE Year = 1994;
+
+SELECT * FROM death_rates_usa WHERE Year = 1995;
+
+SELECT * FROM death_rates_usa WHERE Year = 1996;
+
+SELECT * FROM death_rates_usa WHERE Year = 1997;
+
+SELECT * FROM death_rates_usa WHERE Year = 1998;
+
+SELECT * FROM death_rates_usa WHERE Year = 1999;
+
+SELECT * FROM death_rates_usa WHERE Year = 2000;
+
+SELECT * FROM death_rates_usa WHERE Year = 2001;
+
+SELECT * FROM death_rates_usa WHERE Year = 2002;
+
+SELECT * FROM death_rates_usa WHERE Year = 2003;
+
+SELECT * FROM death_rates_usa WHERE Year = 2004;
+
+SELECT * FROM death_rates_usa WHERE Year = 2005;
+
+SELECT * FROM death_rates_usa WHERE Year = 2006;
+
+SELECT * FROM death_rates_usa WHERE Year = 2007;
+
+SELECT * FROM death_rates_usa WHERE Year = 2008;
+
+SELECT * FROM death_rates_usa WHERE Year = 2009;
+
+SELECT * FROM death_rates_usa WHERE Year = 2010;
+
+SELECT * FROM death_rates_usa WHERE Year = 2011;
+
+SELECT * FROM death_rates_usa WHERE Year = 2012;
+
+SELECT * FROM death_rates_usa WHERE Year = 2013;
+
+select distinct AgeAdjustedDeathRate from death_rates_usa;
+
+select sum(AgeAdjustedDeathRate) as sumofAgeAdjustedDeathRate from death_rates_usa;
+
+select round(sum(AgeAdjustedDeathRate)) as RoundedSumofAgeAdjustedDeathRate from death_rates_usa;
+
+select avg(AgeAdjustedDeathRate) as AvgofAgeAdjustedDeathRate from death_rates_usa;
+
+select round(avg(AgeAdjustedDeathRate)) as RoundedAvgofAgeAdjustedDeathRate from death_rates_usa;
+
+select min(AgeAdjustedDeathRate) as minofAgeAdjustedDeathRate from death_rates_usa;
+
+select round(min(AgeAdjustedDeathRate)) as RoundedMinofAgeAdjustedDeathRate from death_rates_usa;
+
+select max(AgeAdjustedDeathRate) as maxofAgeAdjustedDeathRate from death_rates_usa;
+
+select round(max(AgeAdjustedDeathRate)) as RoundedMaxofAgeAdjustedDeathRate from death_rates_usa;
